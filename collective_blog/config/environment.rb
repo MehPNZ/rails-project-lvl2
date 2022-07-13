12fffe938f9effc65c292968618f538f38dddeb3
Александр Михайлов <meh@MacBook-Air-Aleksandr.local>
2022-07-13 11:39:53 +0300

    deploy

--- collective_blog/config/environment.rb
+++ collective_blog/config/environment.rb
@@ -3,21 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-require_relative 'rollbar'
-
-    notify = lambda do |e|
-      Rollbar.with_config(use_async: false) do
-        Rollbar.error(e)
-      end
-    rescue StandardError
-      Rails.logger.error 'Synchronous Rollbar notification failed.  Sending async to preserve info'
-      Rollbar.error(e)
-    end
-
-    begin
-      Rails.application.initialize!
-    rescue Exception => e
-      notify.call(e)
-      raise
-    end
