65a13974470045aa25ce23cc1e40cd23a8147758
Александр Михайлов <meh@MacBook-Air-Aleksandr.local>
2022-07-21 13:28:43 +0300

    final

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2022_07_21_095517) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "user_id", null: false
+    t.integer "user_id", null: true
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
