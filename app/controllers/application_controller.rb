229fe8f49666e047e93b542d901e66a88254f3e7
Александр Михайлов <meh@MacBook-Air-Aleksandr.local>
2022-07-21 14:42:31 +0300

    fin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,15 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
-  around_action :switch_locale
+  # around_action :switch_locale
 
-  def switch_locale(&action)
-    locale = params[:locale] || I18n.default_locale
+  # def switch_locale(&action)
+  #   locale = params[:locale] || I18n.default_locale
 
-    I18n.with_locale(locale, &action)
-  end
+  #   I18n.with_locale(locale, &action)
+  # end
 
-  def default_url_options
-    { locale: I18n.locale }
-  end
+  # def default_url_options
+  #   { locale: I18n.locale }
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  scope '/(:locale)', locale: /en|ru/ do
-    resources :posts do
-      scope module: 'posts' do
-        resources :comments, shallow: true
-      end
+  # scope '/(:locale)', locale: /en|ru/ do
+  resources :posts do
+    scope module: 'posts' do
+      resources :comments, shallow: true
     end
-
-    resources :posts do
-      scope module: 'posts' do
-        resources :likes, only: %i[create destroy]
-      end
-    end
-
-    root 'posts#index'
-    devise_for :users
   end
+
+  resources :posts do
+    scope module: 'posts' do
+      resources :likes, only: %i[create destroy]
+    end
+  end
+
+  root 'posts#index'
+  devise_for :users
+  # end
 end
